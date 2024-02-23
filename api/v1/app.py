8c08f84a2a84a74733743c3cafb7d9acc76b6203
balagrivine <balagrivine@gmail.com>
2024-02-23 12:50:05 -0800

    Status of yoour api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 
-from flask import Flask, Blueprint
-from models import view
-from app_views import api.v1.views
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
-app_views = Blueprint('app_views', __name__)
-
 app.register_blueprint(app_views)
 
-@app.teardown_appcontext
+"""@app.teardown_appcontext
 def tear_down():
-    """method to handle @app.teardown_appcontext"""
-    storage.close()
+    method to handle @app.teardown_appcontext
+    storage.close()"""
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', host=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
-import jsonify
+from flask import jsonify
 
 @app_views.route('/status')
-def status_code():
+def status():
     """returns a JSON"""
-    return jsonify({"status":"OK"})
+    return jsonify({"status": "OK"})
