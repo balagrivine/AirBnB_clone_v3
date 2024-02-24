3c0714ac5c6e809b626f72580c4131d90adec8a4
balagrivine <balagrivine@gmail.com>
2024-02-23 23:49:09 -0800

    improve storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,16 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def status():
     """returns a JSON"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats')
+def number_objects():
+    """Retrieves the number of each objects by type"""
+    data = models.storage.all(cls)
+    for k, v in data.items():
+        return jsonify(v: storage.count(v))
