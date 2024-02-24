d0cffc02af7b108fb72fe8927dbd268f26c3fac3
balagrivine <balagrivine@gmail.com>
2024-02-24 00:28:04 -0800

    make the app run according to environmrntal variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -16,6 +16,20 @@ def tear_down(self):
     """
     storage.close()
 
+@app.errorhandler(404)
+def error404():
+    """Method to handle 404 error"""
+    return make_response(jsonify({"error": "Not fount"}), 404)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    if getent("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
 
